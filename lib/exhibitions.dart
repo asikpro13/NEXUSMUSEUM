e4a2bad0287536e2278196eb460c399908c30696
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-03-06 17:05:50 +0500

    Переделана фотография на экране Landing

---
+++ lib/exhibitions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Exhibitions extends StatefulWidget {
+  const Exhibitions({super.key});
+
+  @override
+  State<Exhibitions> createState() => _ExhibitionsState();
+}
+
+class _ExhibitionsState extends State<Exhibitions> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
