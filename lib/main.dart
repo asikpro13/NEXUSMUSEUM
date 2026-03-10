5c4204613679bd2b03f1218d461bfbc6a89cace7
yuwnxu <yulduzgebekova@yandex.ru>
2026-03-10 20:30:32 +0500

    Ограничена поворотность приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:nexusmuseum/landing.dart';
+import 'package:flutter/services.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
